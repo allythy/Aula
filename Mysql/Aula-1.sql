5121551c0f4f2c89a150e21398c419f02f91dbcc
Allythy <allythyrenan@gmail.com>
2017-03-17 14:43:04 -0300

    Ajeitando a foreing key

--- Mysql/Aula-1.sql
+++ Mysql/Aula-1.sql
@@ -1,14 +1,16 @@
-create database db_locadora; 
+create database db_locadora;
 
 use db_locadora;
 
 create table tb_filmes(
-	fil_nome varchar(50),
-    fil_codigo int auto_increment primary key,
-    fil_ano int
+  fil_nome varchar(50),
+  fil_codigo int auto_increment primary key,
+  fil_ano int
 );
 
 create table tb_locacao(
-	loc_codigo int auto_increment primary key,
-    loc_tb_filmes int references tb_filmes(fil_codigo)
+  loc_codigo int auto_increment primary key,
+  loc_tb_filmes int,
+  foreign key (loc_tb_filmes) references tb_filmes(fil_codigo)
 );
+
