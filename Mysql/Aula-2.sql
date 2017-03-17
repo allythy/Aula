e0a6224337842e69b9ca408be6f9630f3021baa0
Allythy <allythyrenan@gmail.com>
2017-03-17 14:51:32 -0300

    Ajeitando a foreing key

--- Mysql/Aula-2.sql
+++ Mysql/Aula-2.sql
@@ -3,20 +3,23 @@ create database db_loja;
 use db_loja;
 
 create table tb_marcas(
-	mar_codigo int auto_increment primary key,
-    mar_marca varchar(50) not null
+  mar_codigo int auto_increment primary key,
+  mar_marca varchar(50) not null
 );
 
 create table tb_estados_conserv(
-	est_codigo int auto_increment primary key,
-    est_estado varchar(50) not null
+  est_codigo int auto_increment primary key,
+  est_estado varchar(50) not null
 );
 
 create table tb_carros(
-	car_codigo int auto_increment primary key,
-    car_placa varchar(8) not null,
-    car_ano int not null,
-    car_est_codigo int not null references tb_estados_conserv(est_codigo),
-    car_mar_codigo int not null references tb_marcas(mar_codigo)
-    
+  car_codigo int auto_increment primary key,
+  car_placa varchar(8) not null,
+  car_ano int not null,
+  car_est_codigo int not null,
+  car_mar_codigo int not null,
+  foreign key (car_est_codigo) references tb_estados_conserv(est_codigo),
+  foreign key (car_mar_codigo) references tb_marcas(mar_codigo)
+
 );
+
