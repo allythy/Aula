aac6e08be7c2057a93f3d4376926bde7b62190e5
Allythy <allythyrenan@gmail.com>
2017-03-20 15:11:07 -0300

    Tabuada de Cinco Usando com DoWhile

---
+++ Java/TabuadaDeCincoDoWhile.java
@@ -0,0 +1,18 @@
+
+package tabuadadecincodowhile;
+
+
+public class TabuadaDeCincoDoWhile {
+
+    
+    public static void main(String[] args) {
+      int contador=1;
+     
+        do{
+           System.out.println("5 x "+ contador +" = "+ contador * 5);
+           contador ++;
+        }while (contador<=10);
+    }
+    
+    
+}
