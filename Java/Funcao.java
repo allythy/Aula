d5853ed6d1d18d7f5729d419d6458ed52f1b2ad4
Allythy <allythyrenan@gmail.com>
2017-03-20 00:50:08 -0300

    Comentando o código

--- Java/Funcao.java
+++ Java/Funcao.java
@@ -5,15 +5,15 @@ public class Funcao {
 
     // Função
     static int soma(int n1, int n2){ 
-      int operacao  = n1 +n2;  
+      int operacao = n1 + n2;  
       return operacao;
     }        
   
     
     public static void main(String[] args) { 
         
-        int resultado = soma(5,3);
-        System.out.println("Soma foi:"+ resultado);
+        //int resultado = soma(5,3); <-- Vai guardar o valor em uma variavel 
+        System.out.println("Soma foi:"+ soma(5,3)); // aqui só vai mostrar na tela
 
 
     }
